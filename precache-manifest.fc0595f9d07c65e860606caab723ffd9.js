b64cdc2dfbacb6ed9783daabce495d9880263f3b
Nobel <nobelinderpreet91@gmail.com>
2020-10-09 14:01:55 +1100

    Updates

---
+++ precache-manifest.fc0595f9d07c65e860606caab723ffd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd789391f8a62adebe988e56d116b5c",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "0d230ea45d75e9556701",
+    "url": "/TodoApp/static/css/main.0f48bdb0.chunk.css"
+  },
+  {
+    "revision": "61aeb103a8a5a24921da",
+    "url": "/TodoApp/static/js/2.edabdc60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.edabdc60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d230ea45d75e9556701",
+    "url": "/TodoApp/static/js/main.056db193.chunk.js"
+  },
+  {
+    "revision": "b6b06cfd499b06dee068",
+    "url": "/TodoApp/static/js/runtime-main.2fb20b0f.js"
+  }
+]);
